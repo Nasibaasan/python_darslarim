0661f59a40229d39017de7127ecf34052e24c5f6
Nasibaasan <nasibaasan@icloud.com>
2025-07-24 23:25:36 +0500

    yangi fayllar yuklandi
    Boshlangich nazariya kodlari yuklandi

---
+++ hello-world.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 13 16:46:39 2025
+
+@author: User
+"""
+#3-dars Python print()funksyasi
+#13/8/2025
+#Muallif: Nasiba
+#quyidagi kod Hello worl jumlasini konsolga chiqaradi
+#print("Assalomu \nAlaykum")
+#print("Assalomu\tAlaykum")
+
+#print('Men"DELL"markasidagi notebook sotib oldim') #matn ichida qo'shtirnoqda yozish
+#print("""Odamiy ersang demagil odamiy \nOnikim yo'q xalq gámidin o'zga gámi""")     #agar matnni birdan ortiq qatorga yozmoqchi bo'lsak
+#\n backslash-Qatorni ikkiga bo'lmoqchi bo'lsak    
+#print (2+4*6)        
+#print(15/4)
+#print(15//4)#qoldiqsiz bolish
+#print(2**4) #drajaga ko'arish
+#ism="Abdulloh" #(o'zgaruvchilar(variables))
+#print(ism)
+#ism="Muhammad"
+#print(ism)
+#ism="Anvar"
+#shahar="qo'qon"
+#viloyat="fargona"
+#matn="men 🐿ni yaxshi koraman"
+#print(matn)
+#
+#print("mening ismim" + ism)
+#ism="Nasiba"
+#familiya="Abduvahobova"
+#ism_familiya=f"Mening toliq ismim {ism}+{familiya}"
+
+#String metodlar
+
+#ism='Nasiba'
+#familiya='Abduvahobova'
+#ism_familiya=f'{ism} {familiya}'
+#ism_familiya=ism_familiya.upper()
+#int(ism_familiya.title())
+#print(ism_familiya.capitalize())
+
+#meva='    olma    '
+#print('Men '+ meva.lstrip() + " yaxshi ko'raman")
+#print('Men '+ meva.rstrip() + " yaxshi ko'raman")
+#print('Men '+ meva.strip() + " yaxshi ko'raman")
+
+
+#   Input
+
+#ism=input(' Ismingiz nima?')
+#print('Assalomu alaykum'+ ism)
+
+#ism=input(' Ismingiz nima?\n>>>')
+#print('Assalomu alaykum, '+ ism.title())
+
+   # Sonlar
+#a=20
+#b=5.5
+#temp=36.6
+#print(type(a))
+#aholi_soni=7_000_000
+#print("aholi nsoni:", aholi_soni)
+#x,y,z = 10,5.5,-56
+
+#c=a*b
+
+#d=1/2
+
+#ism="Nasiba"
+#yosh= 25
+#yosh=(yosh)
+#xabar = ism + '' + str(yosh)
+#print(xabar)
+ 
+#t_yil=int(input("Tugílgan yilingizni kiriting: "))
+#yosh=2020- t_yil
+#rint("Siz ", yosh, "yoshda ekansiz")
+
+
+
+
+
+
+
+
+
+users = ['alisher','aziza','yasina','umar']
+login=input('iltimos login tanlang')
+if login.lower() in users:
+    print(f'Kechirasiz login band')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
